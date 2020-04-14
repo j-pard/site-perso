d20623fe4c4634c08d18124116e38cf3cfe1e010
J-Pard <pardons.jonathan@gmail.com>
2020-04-14 17:43:35 +0200

    Landing

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,17 @@
-( () => {
-      
-})();
+const h1 = document.querySelector("h1");
+const subH1 = document.getElementById("sub-h1");
+
+
+const makeItHoverable = (element) => {
+      let Content = element.textContent.toUpperCase().split("");
+      element.textContent = "";
+      Content.forEach(letter => {
+            let span = document.createElement("span");
+            span.textContent = letter;
+            element.appendChild(span);
+      });
+}
+
+makeItHoverable(h1);
+
+subH1.style.width = h1.offsetWidth + "px";
