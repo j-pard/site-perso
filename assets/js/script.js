dba1fe28aaf9d2148fd979664159eb10e0255e3a
J-Pard <pardons.jonathan@gmail.com>
2020-04-22 12:09:00 +0200

    Socials

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,6 +3,8 @@ const subH1 = document.getElementById("sub-h1");
 const btnSubH1 = document.getElementById("btn-sub-h1");
 const cite = document.getElementById("cite");
 const footCite = document.getElementById("foot-cite");
+const header = document.querySelector("header");
+const body = document.querySelector("body");
 
 const makeItHoverable = (element) => {
       let Content = element.textContent.toUpperCase().split("");
@@ -23,3 +25,5 @@ subH1.style.width = h1.offsetWidth + "px";
 btnSubH1.style.width = h1.offsetWidth + "px";
 
 footCite.style.width = cite.offsetWidth + "px";
+
+body.style.paddingTop = header.offsetHeight + "px";
